e9975a6617986ec217df4365e11028e187bc1f89
Gavin McFarland <gavin@mindthetic.com>
2018-04-22 22:32:12 +0100

    New code

--- .tape.js
+++ .tape.js
@@ -3,17 +3,8 @@ module.exports = {
 		'gutters': {
 			message: 'supports gutters property'
 		},
-		'width': {
-			message: 'supports width property'
-		},
-		'height': {
-			message: 'supports height property'
-		},
 		'width.gutters': {
 			message: 'supports width and gutters when applied to same element'
-		},
-		'width.p-gutters': {
-			message: 'supports width and gutters with percentages when applied to same element'
 		}
 	}
 };
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import postcss from "postcss";
-import valueParser from "postcss-value-parser";
+var valueParser = require('postcss-value-parser');
 
 const pf = "--FI_";
 const CS = " > *";
 const SS = " > ::slotted(*)";
 
 
-function addGap(property) {
+function addGap(decl, webComponents) {
 
-	createRules();
+	let value = valueParser.unit(decl.value);
+
+	const container = decl.parent;
+	const item = postcss.rule({selector: container.selector + CS});
+	const reset = postcss.rule({selector: container.selector + CS + CS});
+	const slotted = postcss.rule({selector: container.selector + SS});
+
+	container.before(slotted);
+	container.before(item);
+	item.before(reset);
 
 	// Percentages
-	if (percentage) {
+	if (value.unit === "%") {
 
 		container.append(
-		   `${pf}gap: ${value};
-			${pf}gap_container: ${value};
+		   `${pf}gap: ${value.number};
+			${pf}gap_container: ${value.number};
 			${pf}gap_number: ${value.number};
-			${pf}gap_percentage-decimal: ${value.number} / 100;
-			${pf}gap_negative: -1 * ${value};
+			${pf}gap_percentage-decimal: ${value.number / 100};
+			${pf}gap_negative:  ${-1 * value.number + value.unit};
 			${pf}gap_is-percentages: true;
 			${pf}gap_new: `
 		);
@@ -29,18 +38,17 @@ function addGap(property) {
 		);
 
 		reset.append(
-		   `{pf}gap_difference: initial;
-			{pf}gap_new: initial`
+		   `${pf}gap_difference: initial;
+			${pf}gap_new: initial`
 		);
 
 	}
 
 	// Pixels, Ems
 	else {
-
 		container.append(
-		   `${pf}gap: ${value};
-			${pf}gap_negative: -1 * ${value};
+		   `${pf}gap: ${value.number};
+			${pf}gap_negative: -1 * ${value.number};
 			${pf}gap_is-pixels: true;
 			${pf}gap_parent: var(${pf}gap);
 			${pf}gap_new: calc(var(${pf}gap_parent, 0px) - var(${pf}gap_child, 0px));`
@@ -51,7 +59,7 @@ function addGap(property) {
 			${pf}gap_new: var(${pf}gap_parent);`
 		);
 
-		childContainer.append(
+		reset.append(
 		   `${pf}gap_difference: initial;
 			${pf}gap_new: initial`
 		);
@@ -59,10 +67,10 @@ function addGap(property) {
 	}
 
 	// If web components
-	if (webComponents) {
+	if (webComponents === true) {
 		slotted.append(
 			`${pf}gap: initial;
-			 ${pf}gap_parent: ${decl.value} !important;
+			 ${pf}gap_parent: ${value.number} !important;
 			 ${pf}gap_negative: calc(var(${pf}gap, 0px) - var(${pf}gap_child, 0px)) !important;
 			 ${pf}gap_new: var(${pf}gap, 0px);`
 		);
@@ -74,15 +82,20 @@ function addGap(property) {
 	else {
 		container.append(
 		   `padding-top: 0.02px;
-			margin-top; var(${pf}gap_new);
+			margin-top: var(${pf}gap_new);
 			margin-left: var(${pf}gap_new);`
 		);
 
 		item.append(
-		   `margin-top; var(${pf}gap_new);
+		   `margin-top: var(${pf}gap_new);
 			margin-left: var(${pf}gap_new);`
 		);
 	}
+
+	container.walk(i => { i.raws.before = "\n\t" });
+	item.walk(i => { i.raws.before = "\n\t" });
+	reset.walk(i => { i.raws.before = "\n\t" });
+	slotted.walk(i => { i.raws.before = "\n\t" });
 
 	decl.remove();
 }
@@ -90,73 +103,89 @@ function addGap(property) {
 
 function addWidth(decl) {
 
-	// Percentages
-	if (percentage) {
-		original.before {
-		   `${pf}${prop}: ${value};
-			${pf}${prop}_number: ${value.number};
-			${pf}${prop}_percentage_decimal: ${value.number / 100};
-			${pf}${prop}_is-percentage: ${value};
-			${pf}${prop}_new: calc(${value} + var(${pf}gutters_width, var(${pf}gutters_child-width, var(${pf}gutters, 0px))));`
-		}
+	let value = valueParser.unit(decl.value);
+	let prop = decl.prop;
 
-		reset.before {
-		   `${pf}${prop}: initial;
+	const container = decl.parent;
+	const reset = postcss.rule({selector: container.selector + CS});
+
+	container.before(reset);
+
+	// createRules();
+
+	// Percentages
+	if (value.unit === "%") {
+		container.append(
+		   `${pf}${prop}: ${decl.value};
+			${pf}${prop}_number: ${value.number};
+			${pf}${prop}_percentage_decimal: ${value.number};
+			${pf}${prop}_is-percentage: ${value.number};
+			${pf}${prop}_new: calc(${value.number} + var(${pf}gutters_width, var(${pf}gutters_child-width, var(${pf}gutters, 0px))));`
+		);
+
+		reset.append(
+			`${pf}${prop}: initial;
 			${pf}${prop}_new: initial;`
-		}
+		);
 	}
 
 	// Pixels, Ems
 	else {
-		original.before {
-		   `${pf}${prop}: ${value};
-			${pf}${prop}_is-pixel: ${value};
-			${pf}${prop}_new: calc(${value} + var(${pf}gutters_percentage-width, var(${pf}gutters_pixels, 0px)));;`
-		}
+		container.append(
+		   `${pf}${prop}: ${value.number};
+			${pf}${prop}_is-pixel: ${value.number};
+			${pf}${prop}_new: calc(${value.number} + var(${pf}gutters_percentage-width, var(${pf}gutters_pixels, 0px)));`
+		);
 
-		reset.before {
+		reset.append(
 		   `${pf}${prop}: initial;
 			${pf}${prop}_new: initial;`
-		}
+		);
 	}
 
-	// Both
-	original.append(
-	   `${pf}${prop}; var(${pf}width_new);`
+	decl.before(
+	   `${prop}: var(${pf}${prop}_new);`
 	);
 
 	decl.remove();
-}
 
-function createRules() {
+	// cleanRules();
 
-	if (gutters, width) {
-		const container = decl.parent;
-		const item = postcss.rule({selector: container.selector + CS});
-		const reset = postcss.rule({selector: container.selector + CS + CS});
+	container.walk(i => {i.raws.before = "\n\t";});
+	reset.walk(i => {i.raws.before = "\n\t";});
 
-		container.before(item);
-		item.before(reset);
-	}
-	if (gutters) {
-		if (webComponents) {
-			const slotted = postcss.rule({selector: container.selector + SS});
-			container.before(slotted);
-		}
-	}
 
 }
 
-function cleanRules() {
-	if (gutters, width) {
-		container.walk(i => { i.raws.before = "\n\t" });
-		item.walk(i => { i.raws.before = "\n\t" });
-	}
-	if (gutters) {
-		reset.walk(i => { i.raws.before = "\n\t" });
-		slotted.walk(i => { i.raws.before = "\n\t" });
-	}
-}
+// function createRules() {
+//
+// 	if (gutters, width) {
+// 		const container = decl.parent;
+// 		const item = postcss.rule({selector: container.selector + CS});
+// 		const reset = postcss.rule({selector: container.selector + CS + CS});
+//
+// 		container.before(item);
+// 		item.before(reset);
+// 	}
+// 	if (gutters) {
+// 		if (webComponents) {
+// 			const slotted = postcss.rule({selector: container.selector + SS});
+// 			container.before(slotted);
+// 		}
+// 	}
+//
+// }
+
+// function cleanRules() {
+// 	if (gutters, width) {
+// 		container.walk(i => { i.raws.before = "\n\t" });
+// 		item.walk(i => { i.raws.before = "\n\t" });
+// 	}
+// 	if (gutters) {
+// 		reset.walk(i => { i.raws.before = "\n\t" });
+// 		slotted.walk(i => { i.raws.before = "\n\t" });
+// 	}
+// }
 
 export default postcss.plugin("postcss-gutters", (opts) => {
 	var webComponents = false;
@@ -167,10 +196,10 @@ export default postcss.plugin("postcss-gutters", (opts) => {
 	return function (css) {
 		css.walkDecls(function (decl) {
 			if (decl.prop === "width" || decl.prop === "height") {
-				gutterLengthProp(decl);
+				addWidth(decl);
 			}
 			if (decl.prop === "gutters") {
-				guttersProp(decl, webComponents);
+				addGap(decl, webComponents);
 			}
 		});
 	};
