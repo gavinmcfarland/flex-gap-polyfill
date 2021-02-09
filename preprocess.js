2579e207708906c27c7c10597ad625e014f36c4c
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-02-09 11:55:13 +0000

    Updated preprocess file

--- preprocess.js
+++ preprocess.js
@@ -7,31 +7,38 @@ let options = {
 	processBlockStyles: true
 }
 
-function processPHTML(dir) {
-	// var dir = path.dirname(input);
+var watching = process.argv[2]
 
-	console.log("Watching for changes to phtml files...")
-	chokidar.watch(dir).on('all', (event, filepath) => {
+function processPHTML(event, filepath) {
 
-		if (/.phtml$/.test(filepath)) {
-			let dirname = path.dirname(filepath)
-			let basename = path.basename(filepath, '.phtml')
-			let output = path.join(dirname, basename + '.html');
+	if (/.phtml$/.test(filepath)) {
+		let dirname = path.dirname(filepath)
+		let basename = path.basename(filepath, '.phtml')
+		let output = path.join(dirname, basename + '.html');
 
-			fs.readFile(filepath, 'utf8', (err, html) => {
-				phtmlUtilityClass.process(html, null, options).then((result) => {
-					// console.log(result.html)
-					fs.writeFile(output, result.html, () => true);
-					// if (result.map) {
-					// 	fs.writeFile(output + '.map', result.map, () => true);
-					// }
-				});
+		fs.readFile(filepath, 'utf8', (err, html) => {
+			phtmlUtilityClass.process(html, null, options).then((result) => {
+				// console.log(result.html)
+				fs.writeFile(output, result.html, () => true);
+				console.log("HTML file rebuilt")
+				// if (result.map) {
+				// 	fs.writeFile(output + '.map', result.map, () => true);
+				// }
 			});
-		}
+		});
+	}
 
-	});
 }
-processPHTML('docs/')
+
+// processPHTML('docs/')
+
+if (watching) {
+	console.log("Watching for changes to phtml files...")
+	chokidar.watch('docs/').on('all', processPHTML)
+}
+else {
+	chokidar.watch('docs/', { persistent: false }).on('all', processPHTML)
+}
 
 
 
